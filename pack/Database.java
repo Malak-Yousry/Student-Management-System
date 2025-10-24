24816c05fcab12062150ff1dd8dfdb48a0037295
Malak-Yousry <es.mlk.yousry2023@alexu.edu.eg>
2025-10-24 11:22:21 +0300

    updates on database

--- pack/Database.java
+++ pack/Database.java
@@ -89,6 +89,19 @@ public class Database {
 			}
             return records;
      }
+    //save to file
+    public void saveToFile() {
+		 
+		try(FileWriter writer = new FileWriter(new File(filename))){
+			for(StudentRecord s :students) {
+			writer.write(student.get(i).lineRepresentation() + "\n");
+			
+		}}
+		catch(IOException e) {
+			System.out.println("An error occurred");
+			e.printStackTrace();
+		}
+	}
     // view method
      public void viewAllStudents(){
     ArrayList<StudentRecord> records=readFromFile();
@@ -165,4 +178,21 @@ public class Database {
 
         return confirmDeletion;
     }
+// add students method
+	 public void addStudent(int studentID,String fullName,int age,String gender,String departement,float gpa){
+	    StudentRecord student = new   StudentRecord (studentID,fullName,age,gender,departement,gpa);
+		   setStudentID(studentID);
+		   setFullName(fullName);
+		   setAge(age);
+		   setGender(gender);
+		   setDepartement(departement); 
+		   setGpa(gpa);
+		   students.add(student);
+		   saveToFile();
+
+		 }
+
+	 public String lineRepresentation() {
+		 return getStudentID() + "," + getFullName() + "," + getGender() + "," + getDepartement() + "," +  getGpa();
+	 }
     }
