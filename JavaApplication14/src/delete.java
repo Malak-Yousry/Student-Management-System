393bd9ac21dc76d2b96081e0a73c3c6ebb5a7ee5
HannahEmad <hannahemadeldin@gmail.com>
2025-10-26 23:12:21 +0300

    Deleting

--- JavaApplication14/src/delete.java
+++ JavaApplication14/src/delete.java
@@ -1,44 +1,48 @@
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
 
-public class delete extends javax.swing.JFrame {
 
-    public delete() {
+
+ public class Delete extends javax.swing.JFrame {
+    private JTable table;
+    private DefaultTableModel model;
+    private JButton jButton1;
+    private final String FILE_PATH = "students.txt";
+
+
+    public Delete() {
         initComponents();
+      loadStudentsFromFile();    
+}
+    private void initComponents() {
+        jButton1 = new JButton("Delete");
+        jButton1.addActionListener(this::jButton1ActionPerformed);
+
+        model = new DefaultTableModel(new Object[]{"Student Name"}, 0);
+        table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        add(scrollPane);
+        add(jButton1);
+        pack();
+        setLocationRelativeTo(null);
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
-            },
-            new String [] {
-                "ID", "Fullname", "Age", "Gender", "department", "GPA"
-            }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.Double.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-        });
-        jScrollPane1.setViewportView(jTable1);
 
         jButton1.setText("Delete");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -52,69 +56,96 @@ public class delete extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 330, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(336, 336, 336)
                 .addComponent(jButton1))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(271, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
-        
-        int selectedRow = jTable1.getSelectedRow();
-        
-        if (selectedRow == -1) {
-    JOptionPane.showMessageDialog(this, "Please select what you want to delete!");
-               return;
-        }
-        
-        Object id = model.getValueAt(selectedRow, 0); 
-        
-        String name = model.getValueAt(selectedRow, 1).toString(); 
-        
-         int confirm = JOptionPane.showConfirmDialog(
-                this,
-            "Are you sure you want to delete " + name + " (ID: " + id + ")?",
-            "Confirm Deletion",
-            JOptionPane.YES_NO_OPTION
-         );
-         
-          if (confirm == JOptionPane.YES_OPTION) {
-              model.removeRow(selectedRow);
-       DeletestudentbyID deleter = new DeletestudentbyID();
+    int selectedRow = jTable1.getSelectedRow();
 
-          boolean success = deleter.deleteStudentById((String) id);
+    if (selectedRow != -1) {
+        // Get student ID and Name from the selected row
+        String studentId = jTable1.getValueAt(selectedRow, 0).toString(); // Column 0 = ID
+        String studentIName = jTable1.getValueAt(selectedRow, 1).toString(); // Column 1 = Name
+        // Show confirmation dialog
+        int confirm = JOptionPane.showConfirmDialog(this,
+                "Are you sure you want to delete " + studentName + "?",
+                "Confirm Deletion", JOptionPane.YES_NO_OPTION);
 
-        if (success) {
-            JOptionPane.showMessageDialog(this, "Record deleted successfully!");
-        } else {
-            JOptionPane.showMessageDialog(this, "Failed to delete record from file.");
+        if (confirm == JOptionPane.YES_OPTION) {
+            // Remove from table
+            DefaultTableModel Table = (DefaultTableModel) jTable1.getModel();
+            Table.removeRow(selectedRow);
+            // Remove from file
+            boolean deleted = DeleteStudentById(studentId);
+
+            if (deleted) {
+                JOptionPane.showMessageDialog(this, studentName + " has been removed.");
+            } else {
+                JOptionPane.showMessageDialog(this, "Failed to remove " + studentName + " from file.");
+            }
         }
+    } else {
+        JOptionPane.showMessageDialog(this, "Please select a student to delete.");
     }
 }
+
+
+
+   private void loadStudentsFromFile() {
+    try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] data = line.split(","); 
+            model.addRow(data);
+        }
+    } catch (IOException e) {
+        System.err.println("Error reading file: " + e.getMessage());
+    }
+}
+
+ private void updateStudentFile() {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
+        for (int i = 0; i < model.getRowCount(); i++) {
+            for (int j = 0; j < model.getColumnCount(); j++) {
+                writer.write(model.getValueAt(i, j).toString());
+                if (j < model.getColumnCount() - 1) {
+                    writer.write(",");
+                }
+            }
+            writer.newLine();
+        }
+    } catch (IOException e) {
+        System.err.println("Error writing file: " + e.getMessage());
+    }
+}
+
+        private boolean DeleteStudentById(String studentId) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
    
-   /* public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new delete().setVisible(true);
-            }
-        });
-    }
-    */
+/* public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(() -> {
+                new Delete().setVisible(true);
+    });
+}*/
+
+
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
