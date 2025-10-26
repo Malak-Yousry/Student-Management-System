cb26c95d8f1b4678ead00d07caadfa8c10dc26fc
HannahEmad <hannahemadeldin@gmail.com>
2025-10-26 19:42:55 +0300

    Delete update

--- JavaApplication14/src/delete.java
+++ JavaApplication14/src/delete.java
@@ -90,50 +90,32 @@ public class delete extends javax.swing.JFrame {
          
           if (confirm == JOptionPane.YES_OPTION) {
               model.removeRow(selectedRow);
-               JOptionPane.showMessageDialog(this, "Record deleted successfully!");
-          }
+       DeletestudentbyID deleter = new DeletestudentbyID();
 
+          boolean success = deleter.deleteStudentById((String) id);
+
+        if (success) {
+            JOptionPane.showMessageDialog(this, "Record deleted successfully!");
+        } else {
+            JOptionPane.showMessageDialog(this, "Failed to delete record from file.");
+        }
+    }
+}
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(delete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(delete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(delete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(delete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+   
+   /* public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new delete().setVisible(true);
             }
         });
     }
-    
+    */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
-}
+
